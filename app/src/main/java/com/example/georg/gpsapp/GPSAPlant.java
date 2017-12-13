028f30b9295ff6c80d3179b9398964bc6fe59196
eldok11 <greko12345@hotmail.com>
2017-12-13 21:45:05 +0100

    Step 3 Explicit Intent Camere
    Es wird ein Intant erstellt,
    mit der ersten If struktur, wird geprüft, ob
    man auf ok, gedrückt hat, wird die methode ausgeführt, wenn nicht enthalten und der user klickt nicht auf ok, macht also kein bild, systemabsturz
    
    Note debugging
    auf zeile klicken, dan unter run, toggle button auf zeile setzen, bis dahin wird die app ausgeführt, dann übernehme ich. Dann auf run debug app.
    Ab dem punkt wo ich übernehmen f8 step over f7 step into und f9 lass die app wieder laufen, mit f2 kann ich die values ändern.

--- app/src/main/java/com/example/georg/gpsapp/GPSAPlant.java
+++ app/src/main/java/com/example/georg/gpsapp/GPSAPlant.java
@@ -1,14 +1,20 @@
 package com.example.georg.gpsapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class GPSAPlant extends AppCompatActivity {
 
+    public static final int CAMERA_REQUEST = 10;
     private AutoCompleteTextView actPlantName;
+    private ImageView imgSpecimenPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,4 +37,27 @@ public class GPSAPlant extends AppCompatActivity {
         Toast.makeText(this,plantName,Toast.LENGTH_LONG).show();
 
     }
-}
+
+    //photo buttoen geklickt
+    public void btnTagePhotoClicked(View view) {
+        Intent cameraIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Weenn der user ok gewählt hat un nicht abgebrochen hat.
+        if(resultCode==RESULT_OK){
+        if(requestCode==CAMERA_REQUEST){
+            //We hören zurück von der Kamera
+            Bitmap cameraImage = (Bitmap)data.getExtras().get("data");
+
+            //get access to the image view.
+            imgSpecimenPhoto = findViewById(R.id.imgSpecimenphoto);
+            imgSpecimenPhoto.setImageBitmap(cameraImage);
+
+
+        }
+    }
+}}
